fcb2f9b74b877269088d4dda5c5839962af5bed6
JustOden <>
2024-12-30 21:28:59 -0400

    Last Sync: 2024-12-31 01:28:59 (Mobile)

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
-
+# does this work?
 def main():
     with app.app_context():
         db.create_all()
